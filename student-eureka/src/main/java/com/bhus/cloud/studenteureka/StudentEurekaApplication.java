15b8772c54eb7402aba010ebb55d4994d2a24903
bhushanRoh <rohankarbhushan74@gmail.com>
2021-11-25 11:14:40 +0530

    Student Rest Api

--- student-eureka/src/main/java/com/bhus/cloud/studenteureka/StudentEurekaApplication.java
+++ student-eureka/src/main/java/com/bhus/cloud/studenteureka/StudentEurekaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
 public class StudentEurekaApplication {
 
 	public static void main(String[] args) {
---
+++ student/src/main/java/com/bhus/student/document/StudentDocument.java
@@ -0,0 +1,21 @@
+package com.bhus.student.document;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "student")
+public class StudentDocument {
+    @Id
+    private String studentId;
+    private String studentName;
+}
