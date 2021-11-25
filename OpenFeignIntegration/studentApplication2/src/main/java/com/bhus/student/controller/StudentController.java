b2108dbe5f3b3371b0589ecba662583dcfbce1ea
bhushanRoh <rohankarbhushan74@gmail.com>
2021-11-25 17:19:17 +0530

    OpenFeignStudent integrated

---
+++ OpenFeignIntegration/studentApplication2/src/main/java/com/bhus/student/controller/StudentController.java
@@ -0,0 +1,45 @@
+package com.bhus.student.controller;
+
+import com.bhus.student.dao.StudentDao;
+import com.bhus.student.document.StudentDocument;
+import com.bhus.student.model.StudentRequest;
+import com.bhus.student.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+public class StudentController {
+
+    @Autowired
+    StudentService studentService;
+
+    @Autowired
+    StudentDao studentDao;
+
+
+    @PostMapping("/student")
+    public ResponseEntity<?> createStudent(@RequestBody StudentRequest studentRequest){
+        return new ResponseEntity<>(studentService.saveStudent(studentRequest), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/student/{studentId}")
+    public ResponseEntity<?> updateStudent(@PathVariable String studentId, @RequestBody StudentRequest request){
+        return new ResponseEntity<>(studentService.updateStudent(studentId,request),HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping(path = "/student/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?>getStudentById(@PathVariable final String id){
+        Optional<StudentDocument> studentDocuments=studentDao.findById(id);
+        return new ResponseEntity<>(studentDocuments.get(),HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/student/{studentId}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> deleteStudentById(@PathVariable final String studentId){
+        studentService.deleteById(studentId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT.getReasonPhrase(), HttpStatus.NO_CONTENT);
+    }
+}
---
+++ OpenFeignIntegration/studentApplication2/src/test/java/com/bhus/student/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhus.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
